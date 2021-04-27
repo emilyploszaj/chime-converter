08d57307cb9cd6f6106120913a36b355f222ad26
Sir_Gavith <63616979+SirGavith@users.noreply.github.com>
2021-04-26 22:15:43 -0700

    Armor :eyes:
    Armor works
    I also fixed the last commit
    I also added auto pack.mcmeta pack_version changing for armor to work
    Minor refactor too

--- source/app.d
+++ source/app.d
@@ -10,6 +10,7 @@ import std.string;
 immutable string STUB_MODEL = import("stubModel.json");
 immutable string STUB_OVERRIDE = import("stubOverride.json");
 immutable string STUB_PREDICATE = import("stubPredicate.json");
+immutable string STUB_MCMETA = import("stubmcmeta.mcmeta");
 
 static string[string] forwards;
 
@@ -35,14 +36,16 @@ void main(string[] args) {
 		citDir = filename ~ "/assets/minecraft/mcpatcher/cit";
 	}
 	if (citDir.exists && citDir.isDir) {
-		writefln("Found optifine/mcpatcher folder at: %s", citDir);
+		writefln("Found cit folder at: %s", citDir);
 		writeln("\n--Converting Files--");
 		citDir.dirEntries(SpanMode.depth).each!((string file) {
 			if (file.endsWith(".properties")) {
 				try {
 					attemptConversion(file);
-				} catch (Exception e) {
+				} catch (FileException e) {
 					writeln(e.msg);
+				} catch (Exception e) {
+					writeln(e);
 				}
 			}
 		});
@@ -54,8 +57,18 @@ void main(string[] args) {
 	foreach (Identifier item, Override[] o; knownOverrides) {
 		generateOverride(item, o);
 	}
-	copy(filename ~ "/pack.mcmeta", "out/pack.mcmeta");
-	copy(filename ~ "/pack.png", "out/pack.png");
+	try {
+		string mcmeta = cast(string) read(filename ~ "/pack.mcmeta");
+		int[] desc = findValue(mcmeta, `"description"`);
+		std.file.write("out/pack.mcmeta", STUB_MCMETA.format(mcmeta[desc[0]..desc[1]]));
+	} catch (Exception e) {
+		writeln("Could not copy pack.mcmeta");
+	}
+	try {
+		copy(filename ~ "/pack.png", "out/pack.png");
+	} catch (Exception e) {
+		writeln("Could not copy pack.png");
+	}
 }
 
 void attemptConversion(string file) {
@@ -83,9 +96,6 @@ void attemptConversion(string file) {
 			// Check for aliases
 			if (name in forwards) {
 				name = forwards[name];
-			} else if (matchFirst(splitname[1], 
-				"[leather|golde?n?|iron|diamond|netherite]_[boots|leggings|chestplate|helmet]_overlay")) {
-				name = "texture";
 			}
 			switch(name) {
 				case "type":
@@ -118,11 +128,16 @@ void attemptConversion(string file) {
 					break;
 				default:
 					if (splitname[0] == "texture") {
-						textures ~= ExtraTexture(splitname[1], value);
+						if (type == Type.ITEM && matchFirst(splitname[1], 
+							"(leather|golde?n?|iron|diamond|netherite|chainmail)_(boots|leggings|chestplate|helmet)(_overlay)?")) {
+							texture = value;
+						} else {
+							textures ~= ExtraTexture(splitname[1], value);
+						}
 					} else if (splitname[0] == "nbt") {
 						nbts ~= Nbt(splitname,value);
 					} else {
-						throw new Exception("Unrecognized property %s".format(name));
+						throw new FileException("Unrecognized property %s".format(name));
 					}
 			}
 		}
@@ -132,76 +147,64 @@ void attemptConversion(string file) {
 }
 
 void generatePredicate(ConversionFile cFile) {
-	string subPath = sanitize(cFile.fileName.split("/")[citDir.split("/").length..$-1].join("/"));
-	if (subPath != "") { subPath ~= "/"; }
+	string rawSubPath = cFile.fileName.split("/")[citDir.split("/").length..$-1].join("/");
+	if (rawSubPath != "") { rawSubPath ~= "/"; }
+	string subPath = sanitize(rawSubPath);
+	
 	switch(cFile.type){
+		case Type.BLOCK:
+			cFile.type = Type.ARMOR;
+			goto case;
+		case Type.ARMOR: 
 		case Type.ITEM:
-
 			if (cFile.items.length == 0) {
 				writefln("Missing list of items for %s", cFile.fileName);
 				return;
 			}
-			
-			if (cFile.model.length == 0) {
-				//generate a model
-				cFile.model = generateModel(cFile.items, cFile.texture, cFile.fileName.split('/'), subPath);
-			} else {
-				//custom model
-				try {
-					cFile.model = editModel(cFile.model, cFile.texture, cFile.fileName.split('/'), subPath);
-				} catch (Exception e) {
-					writeln(e);
+			if (cFile.type == Type.ITEM) {
+				if (cFile.model.length == 0) {
+					//generate a model
+					cFile.model = generateModel(cFile.items, cFile.texture, cFile.fileName.split('/'), subPath, cFile.type);
+				} else {
+					//custom model
+					cFile.model = editModel(cFile.model, cFile.texture, cFile.fileName.split('/'), subPath, cFile.type);
 				}
+			} else if (cFile.type == Type.ARMOR) {
+				cFile.items = [];
 			}
-
+			
 			foreach (Identifier item; cFile.items) {
 				if (item.path == "skull") {
 					item.path = "player_head";
 					cFile.damages = [];
 				}
-				string[] predicates;
-				if (cFile.counts.length > 0) {
-					predicates ~= `"count": "%s"`.format(cFile.counts[0].chimeRange);
-				}
-				if (cFile.damages.length > 0) {
-					predicates ~= `"damage": %s`.format(cFile.damages[0].chimeRange);
-				}
-				if (cFile.nbts.length > 0) {
-					predicates ~= cFile.nbts.map!(generatePredicateNbt).array;
-				}
-				if (!(item in knownOverrides)) {
-					knownOverrides[item] = [Override(predicates, cFile.model, cFile.weight, Type.ITEM)];
-				} else {
-					knownOverrides[item] ~= Override(predicates, cFile.model, cFile.weight, Type.ITEM);
-				}
-
-				foreach (ExtraTexture et; cFile.textures) {
-					string[string] predicateAliases = [
-						"fishing_rod_cast": "\"cast\": 1",
-						"bow_pulling_0": `"pulling": 1`,
-						"bow_pulling_1": "\"pulling\": 1,\n\t\t\t\t\"pull\": 0.65",
-						"bow_pulling_2": "\"pulling\": 1,\n\t\t\t\t\"pull\": 0.9",
-					];
-					if (et.texture in predicateAliases) {
-						et.texture = "\t\t\t\t" ~ predicateAliases[et.texture];
+			}
+			string[] predicates;
+			if (cFile.counts.length > 0) {
+				predicates ~= `"count": "%s"`.format(cFile.counts[0].chimeRange);
+			}
+			if (cFile.damages.length > 0) {
+				predicates ~= `"damage": %s`.format(cFile.damages[0].chimeRange);
+			}
+			if (cFile.nbts.length > 0) {
+				predicates ~= cFile.nbts.map!(generatePredicateNbt).array;
+			}
+				if (cFile.type == Type.ITEM) {
+				foreach (Identifier item; cFile.items) {
+					if (!(item in knownOverrides)) {
+						knownOverrides[item] = [Override(predicates, cFile.model, cFile.weight, Type.ITEM)];
 					} else {
-						throw new Exception("Unknown extra texture %s".format(et.texture));
+						knownOverrides[item] ~= Override(predicates, cFile.model, cFile.weight, Type.ITEM);
 					}
-					et.value = et.value.chompPrefix("./").chomp(".png");
-					string modelPath = "%s:item/%s".format(namespace, subPath ~ et.value);
-					generateModel(cFile.items, et.value, cFile.fileName.split('/'), subPath);
-					string[] extraPredicates = predicates;
-					extraPredicates ~=  et.texture;
-
-					knownOverrides[item] ~= Override(extraPredicates, modelPath, cFile.weight, Type.ITEM);
+					if (cFile.type == Type.ITEM) {
+						generateExtraTextureOverrides(cFile, predicates, subPath);
+					}
 				}
-				
+			} else if (cFile.type == Type.ARMOR) {
+				generateExtraTextureOverrides(cFile, predicates, subPath);
 			}
 			break;
-		case "block":	
-		case Type.ARMOR:
-			writefln("Chime converter does not yet support armor overrides, skipping %s", cFile.fileName);
-			break;
+			
 		case Type.ENCHANTMENT:
 			writefln("Chime currently does not support enchantment overrides, skipping %s", cFile.fileName);
 			break;
@@ -214,24 +217,45 @@ void generatePredicate(ConversionFile cFile) {
 	}
 }
 
+void generateExtraTextureOverrides(ConversionFile cFile, string[] predicates, string subPath) {
+	foreach (ExtraTexture et; cFile.textures) {
+		string[string] predicateAliases = [
+			"fishing_rod_cast": "\"cast\": 1",
+			"bow_pulling_0": `"pulling": 1`,
+			"bow_pulling_1": "\"pulling\": 1,\n\t\t\t\t\"pull\": 0.65",
+			"bow_pulling_2": "\"pulling\": 1,\n\t\t\t\t\"pull\": 0.9",
+		];
+		string pred;
+		if (et.texture in predicateAliases) {
+			pred = "\t\t\t\t" ~ predicateAliases[et.texture];
+		} else if (cFile.type == Type.ITEM) {
+			throw new Exception("Unknown extra texture %s".format(et.texture));
+		}
+		et.value = et.value.chompPrefix("./").chomp(".png");
+		if (cFile.type == Type.ITEM) {
+			generateModel(cFile.items, et.value, cFile.fileName.split('/'), subPath, cFile.type);
+		} else if (cFile.type == Type.ARMOR) {
+			pruneCopyTexture(et.value, cFile.fileName.split('/'), subPath, cFile.type);
+		}
+		string[] extraPredicates = predicates;
+		if (pred.length > 0) {
+			extraPredicates ~= pred;
+		}
+		string type = cFile.type;
+		if (type == Type.ARMOR) {
+			type  = "textures/" ~ type;
+		}
+		string path = "%s:%s/%s".format(namespace, type, subPath ~ et.value);
+		knownOverrides[Identifier(et.texture)] ~= Override(extraPredicates, path, cFile.weight,  cFile.type);
+	}
+}
+
 string sanitize(string name) {
 	return name.replaceAll(ctRegex!"[^a-z0-9\\/._-]", "_");
 }
 
-string generateModel(Identifier[] vanillaItems, string texture, string[] path, string subPath) {
-	if (texture.length == 0) {
-		texture = path[$ - 1][0..$ - 11];
-	}
-	texture = chompPrefix(texture, "./");
-	if (!texture.endsWith(".png")) {
-		texture ~= ".png";
-	}
-	try {
-		copyTexture("assets/minecraft/" ~ texture, subPath);
-	} catch(FileException e) {
-		texture = path[0..$ - 1].join('/') ~ '/' ~ texture;
-		copyTexture(texture, subPath);
-	}
+string generateModel(Identifier[] vanillaItems, string texture, string[] path, string subPath, Type type) {
+	texture = pruneCopyTexture(texture, path, subPath, type);
 	
 	string parent = "generated";
 	foreach (Identifier vanillaItem; vanillaItems) {
@@ -251,14 +275,14 @@ string generateModel(Identifier[] vanillaItems, string texture, string[] path, s
 	return "%s:item/%s".format(namespace, subPath ~ itemName);
 }
 
-string editModel(string model, string texture, string[] path, string subPath) {
+string editModel(string model, string texture, string[] path, string subPath, Type type) {
 
 	model = chompPrefix(model, "./");
 	if (!model.endsWith(".json")) {
 		model ~= ".json";
 	}
 	string modelFromPath = (path[0..$ - 1] ~ model).join('/');
-	string modelToPath = generatePath(namespace, "models", "item", subPath)~"%s".format(model);
+	string modelToPath = generatePath(namespace, "models", type, subPath)~"%s".format(model);
 	model = "%s:item/%s%s".format(namespace, subPath, sanitize(model.split('/')[$ - 1][0..$ - 5]));
 	string modelFile = cast(string) read(modelFromPath);
 	int[] texturePos;
@@ -277,20 +301,15 @@ string editModel(string model, string texture, string[] path, string subPath) {
 		} else {
 			texture = modelFromPath[0..modelFromPath.lastIndexOf("/")+1]~"%s.png".format(texture);
 		}
+		copyTexture(texture, subPath, type);
 	} else {
-		texture = texture.chompPrefix("./").chomp(".png")~".png";
-		if (texture.canFind('/')) {
-			texture = "%s/assets/minecraft/%s".format(filename,texture);
-		} else {
-			texture = (path[0..$ - 1] ~ texture).join('/');
-		}
+		texture = pruneCopyTexture(texture, path, subPath, type);
 	}
-	copyTexture(texture, subPath);
 	texture = texture.split("/")[$ - 1].chomp(".png");
 	string editedModel = modelFile[0..texturePos[0]] ~
-		"%s:item/%s".format(namespace, subPath ~ texture) ~
+		"%s:%s/%s".format(namespace, type.toLower, subPath ~ texture) ~
 		modelFile[texturePos[1]..$];
-	std.file.write(modelToPath,editedModel);
+	std.file.write(modelToPath, editedModel);
 	
 	return model;
 }
@@ -306,9 +325,32 @@ int[] findValue(string file, string prop) {
 	}
 }
 
-void copyTexture(string texture, string subPath) {
+string pruneCopyTexture(string texture, string[] path, string subPath, Type type) {
+	if (texture.length == 0) {
+		texture = path[$ - 1][0..$ - 11];
+	}
+	texture = chompPrefix(texture, "./");
+	if (!texture.endsWith(".png")) {
+		texture ~= ".png";
+	}
+	string texturepath = texture;
+	try {
+		copyTexture("assets/minecraft/" ~ texturepath, subPath, type);
+	} catch(FileException e) {
+		try {
+			texturepath = (path[0..$ - 1] ~ texture).join('/');
+			copyTexture(texturepath, subPath, type);
+		} catch (FileException fe) {
+			throw new FileException("Could not find texture %s".format(texture));
+		}
+	}
+	return texture;
+}
+
+void copyTexture(string texture, string subPath, Type type) {
 	string itemName = sanitize(texture.split('/')[$ - 1].chomp(".png"));
-	string path = generatePath(namespace, "textures", "item", subPath) ~ "%s.png".format(itemName);
+	string path = generatePath(namespace, "textures", type, subPath) ~ "%s.png".format(itemName);
+
 	if (itemName == "null") {
 		writefln("Skipping copying "~texture);
 		return;
@@ -320,13 +362,22 @@ void copyTexture(string texture, string subPath) {
 }
 
 void generateOverride(Identifier item, Override[] overrides) {
-	writefln("Generating Override %s:%s", item.namespace, item.path);
+	//writefln("Generating Override %s:%s", item.namespace, item.path);
 	sort!((a,b)=>a.weight < b.weight)(overrides);
-	std.file.write(generatePath(item.namespace, "overrides", "item")~"%s.json".format(item.path), STUB_OVERRIDE
+	std.file.write(generatePath(item.namespace, "overrides", overrides[0].type)~"%s.json".format(item.path), STUB_OVERRIDE
 		.format(overrides
 			.map!(o => STUB_PREDICATE
-				.format(o.predicates.join(",\n"), "model", o.model))
+				.format(o.predicates.join(",\n"), overrideType(o.type), o.path))
 			.array.join(",\n")));
+}
+
+string overrideType(string type) {
+	if (type == Type.ITEM) {
+		return "model";
+	} else if (type == Type.ARMOR) {
+		return "texture";
+	}
+	throw new Exception("Unknown type %s".format(type));
 }
 
 string generatePredicateNbt(Nbt nbt) {
@@ -360,7 +411,7 @@ string generatePath(string nspace, string pathType, string type, string subPath 
 
 struct Override {
 	string[] predicates;
-	string model;
+	string path;
 	int weight;
 	Type type;
 }
@@ -426,5 +477,6 @@ enum Type : string {
 	ITEM = "item",
 	ENCHANTMENT = "enchantment",
 	ARMOR = "armor",
-	ELYTRA = "elytra"
+	ELYTRA = "elytra",
+	BLOCK = "block"
 }
